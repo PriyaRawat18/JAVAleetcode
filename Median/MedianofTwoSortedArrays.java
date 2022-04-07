778a0020eaf1a9312d734afebed71cce878a9f45
PriyaRawat18 <85844807+PriyaRawat18@users.noreply.github.com>
2022-04-07 20:18:58 +0530

    Create MedianofTwoSortedArrays.java

---
+++ Median/MedianofTwoSortedArrays.java
@@ -0,0 +1,47 @@
+/*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.*/
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int a[] = new int[m+n];
+        int i = 0, j = 0, k= 0;
+        while(i< m && j<n){
+            if (nums1[i] < nums2[j])
+            {
+                a[k] = nums1[i];
+                i++;
+            }
+            else{
+                a[k]=nums2[j];
+                j++;
+            }
+            k++;
+        }
+        if ((m=n)%2 != 0){
+            return a[(m+n)/2];
+        }
+        else{
+            int x = (m+n)/2;
+            double p = a[x];
+            double q = a[x-1];
+            return (p+q)/2;
+        }
+        }
+        
+    }
